10c298a7503de369fd69c919ceca24edec3ec849
sheezu900 <shahzamanlive2@gmail.com>
2023-03-26 22:08:32 +0500

    try to upload project to github

--- src/accordion.jsx
+++ src/accordion.jsx
@@ -36,3 +36,9 @@ function Accordion({ items }) {
 }
 
 export default Accordion;
+
+// git init
+// git add -A
+// git commit -m 'Added my project'
+// git remote add origin git@github.com:sammy/my-new-project.git
+// git push -u -f origin main
